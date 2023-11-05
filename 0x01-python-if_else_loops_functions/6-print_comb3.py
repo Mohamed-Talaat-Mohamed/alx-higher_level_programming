f204ecabee1bc93314110e505369408d6191ddf3
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-11-05 07:15:43 -0800

    hotfix to t.6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for odd in range(0, 10):
     for even in range(0, 10):
         if odd != even and odd < even:
-           if odd == 8 and even == 9:
-               print(89)
-           else:
-               print("{}{},".format(odd, even), end=" ")
+            if odd == 8 and even == 9:
+                print(89)
+            else:
+                print("{}{},".format(odd, even), end=" ")
