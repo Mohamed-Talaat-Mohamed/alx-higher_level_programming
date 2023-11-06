cca2ed0bebdd8544c65bdbe9f8a3aa120a9fe932
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-11-06 03:56:00 -0800

    hotfix t.6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return None
     for x in matrix:
-        if len(x) == 0:
-            print()
-        for y in range(len(x)):
-            print("{:d}".format(x[y]), end="\n" if y is len(x) - 1 else" ")
+        counter = 0
+        for y in x:
+            counter += 1
+            print('{:d}'.format(y), end=(" " if counter < len(x) else ""))
+        print()
