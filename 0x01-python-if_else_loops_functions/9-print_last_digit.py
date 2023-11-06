5c6d244f9e224ca98ececbcacbd44287c4b61406
Mohamed-Talaat-Mohamed <mohamed.talaat@flo-egypt.com>
2023-11-06 02:35:50 -0800

    Uploading 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 1:
-    l_d = abs(number) % 10
+        number = -1 * number
+    dig = number % 10
     if number < 1:
-        l_d = abs(l_d)
-    print(l_d, end="")
-    return l_d
+        dig = -1 * dig
+    print(dig, end="")
+    return dig
